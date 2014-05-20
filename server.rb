6db6af5e66842224164d026c47fd23b46ef4e1f2
David Pointeau <david.pointeau@tufts.edu>
2014-05-19 22:41:13 -0400

    Shmoogle First Run

---
+++ server.rb
@@ -0,0 +1,7 @@
+require "sinatra"
+
+#to run Shmoogle in a browser.
+#Run the program like you normally run a ruby program: "ruby server.rb"
+#then in your brower open "localhost:4567/index.html"
+#Make sure that your total executable file contains
+#"server.rb" and "public" directory where your .html and .css are located
